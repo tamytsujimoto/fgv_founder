17545c92a0d6e4459796e2f3b195589e8b224d80
Tamy Tsujimoto <tamy.tsujimoto@gmail.com>
2020-05-15 17:00:04 -0400

    Analysis adjusting for industry

--- 2 - Códigos/1 - Data characterization.R
+++ 2 - Códigos/1 - Data characterization.R
@@ -1,31 +1,16 @@
 require(tidyverse)
 require(ggplot2)
+library(viridis)
 
 ibrx <- 
-  read.csv("../1 - Bases de dados/ibrx_db.csv", stringsAsFactors = FALSE) %>% 
-  filter(ibrx == 1,
-        !(empresa %in% c('BBDC3',
-                       'CMIG3',
-                       'ELET3',
-                       'ELPL3',
-                       'GGBR3',
-                       'ITUB3',
-                       'LAME3',
-                       'OIBR3',
-                       'PETR3',
-                       'SUZB3',
-                       'TNPL3',
-                       'USIM3',
-                       'VALE5'))) %>% 
-  mutate(empresa = recode(empresa, 
-                          '0927042D' = 'CCXC3',
-                          'GOAU4'    = 'GGBR3',
-                          'RUMO3'    = 'RAIL3',
-                          'GETI4'    = 'TIET11'),
-         ceo_chair = ifelse(ceo + chairman > 0, 1, 0)) 
+  read.csv("../1 - Bases de dados/ibrx_db_12052020.csv", stringsAsFactors = FALSE) %>% 
+  mutate(ceo_chair = ifelse(ceo + chairman > 0, 1, 0)) 
 
-write.csv(ibrx, 'Output/base_dados_08052020.csv', row.names = FALSE) #sem o filtro ibrx == 1
+########################################################################################################
+# TABLES
+########################################################################################################
 
+# Frequency
 summarytools::freq(ibrx)
 
 # Number of companies
@@ -33,7 +18,9 @@ ibrx %>%
   group_by(empresa) %>% 
   summarise(n=n())
 
-# Number of companies by type of ceo,chairman
+##############################
+# FREQUENCY TYPE OF PORTFOLIO
+##############################
 
 ceo_type <-
   ibrx %>% 
@@ -47,7 +34,20 @@ ceo_type <-
 
 summarytools::freq(ceo_type$ceo_type)
 
-# Summary table
+#####################
+# FREQUENCY INDUSTRY
+#####################
+
+ibrx %>% 
+  group_by(empresa, setor) %>% 
+  summarise(n = n()) %>% 
+  ungroup %>% 
+  select(setor) %>% 
+  summarytools::freq()
+
+############################
+# SUMMARY TYPE PF PORTFOLIO
+############################
 
 tab_ceo = 
   ibrx %>% 
@@ -59,12 +59,92 @@ tab_ceo =
 tab_tot = 
   ibrx %>% 
   group_by(ano) %>% 
-  summarise(n=n(), mean = mean(retorno_anual, na.rm = TRUE), sd = sd(retorno_anual, na.rm = TRUE)) %>% 
+  summarise(n=n(), 
+            mean = round(mean(retorno_anual, na.rm = TRUE),2), 
+            sd = round(sd(retorno_anual, na.rm = TRUE),2)) %>% 
   mutate(ceo_chair = 2) %>% 
   pivot_longer(-c(ceo_chair, ano), names_to = "stat", values_to = "value") %>% 
   pivot_wider(names_from = ano, values_from = value)
 
 write.csv(bind_rows(tab_ceo, tab_tot), file = "Output/charac_tab.csv", row.names = FALSE)
+
+#######################################
+# SUMMARY TYPE PF PORTFOLIO x INDUSTRY
+#######################################
+
+tab_setor = 
+  ibrx %>% 
+  group_by(setor, ano) %>% 
+  summarise(n=n(), 
+            mean = round(mean(retorno_anual, na.rm = TRUE),2), 
+            sd = round(sd(retorno_anual, na.rm = TRUE),2)) %>% 
+  group_by(ano) %>% 
+  mutate(freq = round(n/sum(n)*100,1)) %>% 
+  pivot_longer(c(n, freq, mean, sd), names_to = "stat", values_to = "value") %>% 
+  pivot_wider(names_from = ano, values_from = value)
+
+write.csv(bind_rows(tab_setor, tab_tot), file = "Output/tab_setor.csv", row.names = FALSE)
+
+tab_ceo_setor = 
+  ibrx %>% 
+  group_by(setor, ceo_chair, ano) %>% 
+  summarise(n=n(), 
+            mean = round(mean(retorno_anual, na.rm = TRUE),2), 
+            sd = round(sd(retorno_anual, na.rm = TRUE),2)) %>% 
+  group_by(ceo_chair, ano) %>% 
+  mutate(freq = round(n/sum(n)*100,1)) %>% 
+  pivot_longer(c(n, freq, mean, sd), names_to = "stat", values_to = "value") %>% 
+  unite(ano_ceo, ano, ceo_chair) %>% 
+  pivot_wider(names_from = ano_ceo, values_from = value)
+
+write.csv(tab_ceo_setor, file = "Output/tab_ceo_setor.csv", row.names = FALSE)
+
+########################################################################################################
+# PLOTS
+########################################################################################################
+
+yr = c("09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19")
+pd <- position_dodge(.1)
+ceo_type.lab <- c("Successor only", "Founder only", "Both")
+names(ceo_type.lab) <- c("0", "1", "2")
+ceo_chair.lab <- c("Successor", "Founder")
+names(ceo_chair.lab) <- c("0", "1")
+
+# Industry distribution
+
+ibrx %>% 
+  group_by(ano, setor) %>% 
+  summarise(n = n()) %>% 
+  group_by(ano) %>% 
+  mutate(freq = n/sum(n)*100,
+         y_pos = 102-cumsum(freq)+.7*freq) %>% 
+  ggplot(aes(x=ano, y=freq, fill = setor)) +
+  geom_bar(position="stack", stat="identity") +
+  geom_text(aes(y=y_pos, label=round(freq,1)), vjust=1.6, color="white", size=3.5) +
+  labs(title="", x="Year", y="%", fill="") +
+  theme_bw() +
+  scale_x_continuous(breaks=seq(2009, 2019, by = 1), labels = yr)  +
+  theme(legend.position='top', panel.grid.minor = element_blank())
+
+ggsave('Output/plot_bar_setor.pdf', width = 9.5, height = 6)
+
+ibrx %>% 
+  group_by(ano, ceo_chair, setor) %>% 
+  summarise(n = n()) %>% 
+  group_by(ano, ceo_chair) %>% 
+  mutate(freq = n/sum(n)*100,
+         y_pos = 102-cumsum(freq)+.7*freq) %>% 
+  ggplot(aes(x=ano, y=freq, fill = setor)) +
+  geom_bar(position="stack", stat="identity") +
+  geom_text(aes(y=y_pos, label=round(freq,1)), vjust=1.6, color="white", size=3.5) +
+  #scale_fill_viridis(discrete = TRUE) +
+  facet_grid(~ ceo_chair,  labeller = labeller(ceo_chair = ceo_chair.lab)) +
+  labs(title="", x="Year", y="%", fill="") +
+  theme_bw() +
+  scale_x_continuous(breaks=seq(2009, 2019, by = 1), labels = yr) +
+  theme(legend.position='top', panel.grid.minor = element_blank())
+
+ggsave('Output/plot_bar_setor_ceo.pdf', width = 9.5, height = 6)
 
 # PLOT: Number of observations
 
@@ -81,19 +161,11 @@ ibrx %>%
   scale_x_continuous(name ="Number of timepoints", breaks=c(1:11)) + 
   scale_y_continuous(name ="Frequency of companies")
 
-ibrx %>% 
-  group_by(empresa) %>% 
-  summarise(n = n()) %>% 
-  filter(n > 11)
-
 ggsave('Output/plot_bar.pdf', width = 8, height = 4.5)
 
 ########################  
 # PLOT: Spaguetii plot #
 ########################
-
-yr = c("09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19")
-pd <- position_dodge(.1)
 
 # MEAN PLOTS
 plot_tot <- 
@@ -129,22 +201,54 @@ ggpubr::ggarrange(plot_tot, plot_ceo, ncol=2, nrow=1,
                   legend="top")
 ggsave('Output/plot_perfis.pdf', width = 8, height = 4.5)
 
-# BY TYPE OF CEO
 
-ceo_type.lab <- c("Successor only", "Founder only", "Both")
-names(ceo_type.lab) <- c("0", "1", "2")
-
-ibrx %>% 
-  left_join(ceo_type, by = 'empresa') %>% 
-  ggplot(aes(x = ano, y = retorno_anual, group = empresa, color = factor(ceo_chair))) +
+# INDUSTRY
+plot_tot_setor <- 
+  ibrx %>% 
+  ggplot(aes(x = ano, y = retorno_anual, group = empresa, color = factor(setor))) +
   geom_line() +
   geom_point() +
-  facet_grid(. ~ ceo_type, labeller = labeller(ceo_type = ceo_type.lab)) +
-  scale_color_discrete(name="", labels = c("Successor", "Founder")) +
+  scale_color_discrete(name="") +
   scale_x_continuous(name ="Year", breaks=seq(2009, 2019, by = 1), labels = yr) + 
   scale_y_continuous(name ="Annual stock market return", limits = c(-1,6)) + 
   theme_bw() +
-  theme(legend.position = 'top', plot.margin = unit(c(.1,.05,.1,.1), "cm"))
+  theme(plot.margin = unit(c(.1,.05,.1,.1), "cm"))
 
-ggsave('Output/plot_perfis_ceotype.pdf', width = 8, height = 4.5)
+plot_setor <-
+  ibrx %>% 
+  group_by(ano,setor) %>% 
+  summarise(mean = mean(retorno_anual, na.rm = T),
+            sd = sd(retorno_anual, na.rm = T),
+            n = sum(!is.na(retorno_anual), na.rm = T),
+            ci = sd/sqrt(n)) %>% 
+  ggplot(aes(x = ano, y = mean, color = factor(setor))) +
+  geom_errorbar(aes(ymin = mean-ci, ymax = mean+ci), width = 1, position = pd) +
+  geom_line(position = pd) +
+  geom_point(position = pd) +
+  scale_y_continuous(name ="", limits = c(-1,6)) + 
+  scale_x_continuous(name ="Year", breaks=seq(2009, 2019, by = 1), labels = yr) + 
+  theme_bw() +
+  scale_color_discrete(name="") +
+  theme(legend.position='top', panel.grid.minor = element_blank())
 
+ggpubr::ggarrange(plot_tot_setor, plot_setor, ncol=2, nrow=1, 
+                  common.legend = TRUE, 
+                  legend="top")
+ggsave('Output/plot_perfis_setor.pdf', width = 8, height = 4.5)
+
+# ibrx %>% 
+#   group_by(ano,setor,ceo_chair) %>% 
+#   summarise(mean = mean(retorno_anual, na.rm = T),
+#             sd = sd(retorno_anual, na.rm = T),
+#             n = sum(!is.na(retorno_anual), na.rm = T),
+#             ci = sd/sqrt(n)) %>% 
+#   ggplot(aes(x = ano, y = mean, color = factor(setor))) +
+#   geom_errorbar(aes(ymin = mean-ci, ymax = mean+ci), width = 1, position = pd) +
+#   geom_line(position = pd) +
+#   geom_point(position = pd) +
+#   facet_grid(~ ceo_chair,  labeller = labeller(ceo_chair = ceo_chair.lab)) +
+#   scale_y_continuous(name ="", limits = c(-1,6)) + 
+#   scale_x_continuous(name ="Year", breaks=seq(2009, 2019, by = 1), labels = yr) + 
+#   theme_bw() +
+#   scale_color_discrete(name="") +
+#   theme(legend.position='top', panel.grid.minor = element_blank())
--- 2 - Códigos/2 - Linear Mixed Model.R
+++ 2 - Códigos/2 - Linear Mixed Model.R
@@ -3,29 +3,8 @@ require(ggplot2)
 require(nlme)
 
 ibrx <- 
-  read.csv("../1 - Bases de dados/ibrx_db.csv", stringsAsFactors = FALSE) %>% 
-  filter(ibrx == 1,
-         !(empresa %in% c('BBDC3',
-                          'CMIG3',
-                          'ELET3',
-                          'ELPL3',
-                          'GGBR3',
-                          'ITUB3',
-                          'LAME3',
-                          'OIBR3',
-                          'PETR3',
-                          'SUZB3',
-                          'TNPL3',
-                          'USIM3',
-                          'VALE5'))) %>% 
-  mutate(empresa2 = recode(empresa, 
-                           '0927042D' = 'CCXC3',
-                           'SUZB5'    = 'FIBR3',
-                           'GOAU4'    = 'GGBR3',
-                           'RUMO3'    = 'RAIL3',
-                           'GETI4'    = 'TIET11',
-                           'TCSL4'    = 'TIMP3'),
-         ceo_chair = ifelse(ceo + chairman > 0, 1, 0),
+  read.csv("../1 - Bases de dados/ibrx_db_12052020.csv", stringsAsFactors = FALSE) %>% 
+  mutate(ceo_chair = ifelse(ceo + chairman > 0, 1, 0),
          ano_adj = ano - 2009) %>% 
   filter(complete.cases(.))
 
@@ -33,41 +12,56 @@ ibrx <-
 
 fit1 <- 
   ibrx %>% 
-  lme(retorno_anual ~ factor(ceo_chair) + ano_adj, random = ~ 1 | empresa, data=., method = "ML")
+  lme(retorno_anual ~ factor(ceo_chair) + factor(setor) + ano_adj, random = ~ 1 | empresa, data=., method = "ML")
 summary(fit1)
 
 fit2 <- 
   ibrx %>% 
-  lme(retorno_anual ~ factor(ceo_chair) + ano_adj + I(ano_adj^2), random = ~ 1 | empresa, data=., method = "ML")
+  lme(retorno_anual ~ factor(ceo_chair) + factor(setor) + ano_adj + I(ano_adj^2), random = ~ 1 | empresa, data=., method = "ML")
 summary(fit2)
 
-#anova(fit1, fit2) fit the models using "ML"
+anova(fit1, fit2) #fit the models using "ML"
 
 fit.final <- 
   ibrx %>% 
-  lme(retorno_anual ~ factor(ceo_chair) + ano_adj + I(ano_adj^2), random = ~ 1 | empresa, data=.)
+  lme(retorno_anual ~ factor(ceo_chair) + factor(setor) + ano_adj + I(ano_adj^2), random = ~ 1 | empresa, data=.)
 summary(fit.final)
 
 write.csv(summary(fit.final)$tTable, file = 'Output/fit_final.csv')
 
 # Plotting final model
-yr = c("09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19")
+# yr = c("09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19")
+# 
+# f1 <- function(x) fit.final$coefficients$fixed[1] + fit.final$coefficients$fixed[3]*x + fit.final$coefficients$fixed[4]*x^2
+# f2 <- function(x) sum(fit.final$coefficients$fixed[1:2]) + fit.final$coefficients$fixed[3]*x + fit.final$coefficients$fixed[4]*x^2
+# 
+# ibrx %>% 
+#   mutate(ano_adj = ano - 2009) %>% 
+#   ggplot(aes(x = ano_adj, y = retorno_anual, group = empresa, color = factor(ceo_chair))) +
+#   geom_line() +
+#   geom_point() +
+#   scale_color_discrete(name="", labels = c("Successor", "Founder")) +
+#   scale_x_continuous(name ="Year", breaks=seq(0, 10, by = 1), labels = yr) + 
+#   scale_y_continuous(name ="Annual stock market return", limits = c(-1,6)) + 
+#   theme_bw() +
+#   theme(legend.position = 'top', plot.margin = unit(c(.1,.05,.1,.1), "cm")) +
+#   stat_function(fun = f1, colour = "deeppink4", lwd = 1.5) +
+#   stat_function(fun = f2, colour = "deepskyblue4", lwd = 1.5)
+# 
+# ggsave('Output/plot_model.pdf', width = 8, height = 4.5)
 
-f1 <- function(x) fit.final$coefficients$fixed[1] + fit.final$coefficients$fixed[3]*x + fit.final$coefficients$fixed[4]*x^2
-f2 <- function(x) sum(fit.final$coefficients$fixed[1:2]) + fit.final$coefficients$fixed[3]*x + fit.final$coefficients$fixed[4]*x^2
+##########################
+# PREDICTED TABLE
 
-ibrx %>% 
-  mutate(ano_adj = ano - 2009) %>% 
-  ggplot(aes(x = ano_adj, y = retorno_anual, group = empresa, color = factor(ceo_chair))) +
-  geom_line() +
-  geom_point() +
-  scale_color_discrete(name="", labels = c("Successor", "Founder")) +
-  scale_x_continuous(name ="Year", breaks=seq(0, 10, by = 1), labels = yr) + 
-  scale_y_continuous(name ="Annual stock market return", limits = c(-1,6)) + 
-  theme_bw() +
-  theme(legend.position = 'top', plot.margin = unit(c(.1,.05,.1,.1), "cm")) +
-  stat_function(fun = f1, colour = "deeppink4", lwd = 1.5) +
-  stat_function(fun = f2, colour = "deepskyblue4", lwd = 1.5)
-
-ggsave('Output/plot_model.pdf', width = 8, height = 4.5)
-
+data.frame(
+    ano_adj = rep(0:10, 20),
+    setor = rep(names(table(ibrx$setor)), 2, each = 11),
+    ceo_chair = c(rep(0,110), rep(1,110))
+    ) %>% 
+  mutate(pred = predict(fit.final, newdata = ., level = 0),
+         yr = paste0('yr_', ano_adj)) %>% 
+  pivot_wider(id_cols = c(setor, ceo_chair),
+              names_from = yr,
+              values_from = pred) %>% 
+  write.csv(file = 'Output/fit_final_pred.csv', row.names = FALSE)
+  
